9eb79365c7ece10510a29c166fb133f88124b758
lucasrbrusu <elucas2005@icloud.com>
2026-03-01 14:40:18 +0000

    config anon key

--- admin/js/config.js
+++ admin/js/config.js
@@ -1,4 +1,4 @@
 
-export const SUPABASE_URL = 'https://YOUR-PROJECT-REF.supabase.co';
-export const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+export const SUPABASE_URL = 'https://ueiptamivkuwhswotwpn.supabase.co';
+export const SUPABASE_ANON_KEY = 'sb_publishable_xwvO6KpSuJbKAUNMkV7vrw_S5RN2_XE';
 export const FUNCTION_BASE_URL = `${SUPABASE_URL}/functions/v1`;
